8dc81199029cbcf19988b4d72a1f2283c53db1e3
Slava Zakharov <90175094+petyapetr@users.noreply.github.com>
2025-11-23 00:01:23 +0400

    Add dark mode theme switch

---
+++ main.js
@@ -0,0 +1,34 @@
+const styleLight = document.querySelector(
+	"link[rel=stylesheet][media*=prefers-color-scheme][media*=light]"
+);
+const styleDark = document.querySelector(
+	"link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]"
+);
+
+const definedTheme = globalThis.localStorage.getItem("theme");
+if (definedTheme !== null) {
+	switchScheme(definedTheme);
+}
+
+globalThis.document
+	.querySelector("[data-theme-switch]")
+	.addEventListener("click", () => {
+		if (styleLight.media === "all") {
+			switchScheme("dark");
+			return;
+		}
+		if (styleDark.media === "all") {
+			switchScheme("light");
+			return;
+		}
+		const schema = globalThis.matchMedia("(prefers-color-scheme: dark)").matches
+			? "light"
+			: "dark";
+		switchScheme(schema);
+	});
+
+function switchScheme(scheme) {
+	styleLight.media = scheme === "light" ? "all" : "not all";
+	styleDark.media = scheme === "dark" ? "all" : "not all";
+	globalThis.localStorage.setItem("theme", scheme);
+}
